02de062acb1db1cb1bd544eb76f94301bb057ffb
flaviocarvalho <flavio046@hotmail.com>
2016-05-26 21:14:09 -0500

    finish most of the features. TODO: video modal, duplicate streamers, fix search dropdown

--- js/app.js
+++ js/app.js
@@ -1,6 +1,90 @@
 var App = (function(){
-	function ajaxCall( api, streamer ){
 
+	var duplicate = [];
+	var streamers = [];
+	var searchResults = [];
+	var getStreamsLocal = JSON.parse(localStorage.getItem("streams"));
+
+	function render(status, data){
+		if(status === 'online'){
+			var online = {
+				logo: data.stream.channel.logo,
+				displayName: data.stream.channel.display_name,
+				viewers: data.stream.viewers,
+				game: data.stream.channel.game.toUpperCase(),
+				status: data.stream.channel.status,
+				followers: data.stream.channel.followers,
+				language: data.stream.channel.language.toUpperCase(),
+				views: data.stream.channel.views,
+				channelUrl: data.stream.channel.url
+			};
+			var checkUrl = '';
+			if(typeof online.channelUrl !== 'undefined'){
+				checkUrl = online.channelUrl;
+			}
+			var checkGame = '';
+			if(typeof online.channelGame !== 'undefined'){
+				checkGame = online.channelGame;
+			}
+			var html = 
+			'<li class="collection-item avatar row">' +
+			'<a href="' + checkUrl + '" target="_blank"><img src="' + online.logo + '" alt="" class="preview col s2"></a>' +
+			'<div class="content-row col s5">' +
+			'<span id="title">' + online.displayName + '</span>' +
+			'<p><b>Viewers: </b>' + online.viewers + '</p>' +
+			'<p><b>Game: </b>' + checkGame + '</p>' +
+			'<p class="status truncate">' + online.status + '</p>' +
+			'</div>' +
+			'<div class="content-row second-content col s5">' +
+			'<p><b>Followers: </b>' + online.followers + '</p>' +
+			'<p><b>Language: </b>' + online.language + '</p>' +
+			'<p><b>Views: </b>' + online.views + '</p>' +
+			'<a id="close-btn" href="#"><i id="close" class="material-icons">close</i></a>' +
+			'<a class="secondary-content"><i class="online material-icons">play_circle_filled</i></a>' +
+			'</div>' +
+			'</li>';
+			$('.collection').append(html);
+		}else if(status === 'offline'){
+			var offline = {
+				logo: data.logo,
+				displayName: data.display_name,
+				game: data.game.toUpperCase(),
+				status: data.status,
+				followers: data.followers,
+				language: data.language.toUpperCase(),
+				views: data.views,
+				channelUrl: data.url
+			};
+			var checkUrl = '',
+				checkGame = '';
+			if(typeof offline.channelUrl !== 'undefined'){
+				checkUrl = offline.channelUrl;
+			}
+			if(typeof offline.channelGame !== 'undefined'){
+				checkGame = offline.channelGame;
+			}
+			var html = 
+			'<li class="collection-item avatar row">' +
+			'<a href="' + checkUrl + '" target="_blank"><img src="' + offline.logo + '" alt="" class="preview col s2"></a>' +
+			'<div class="content-row col s5">' +
+			'<span id="title">' + offline.displayName + '</span>' +
+			'<p><b>Game: </b>' + checkGame + '</p>' +
+			'<p class="status truncate">' + offline.status + '</p>' +
+			'</div>' +
+			'<div class="content-row second-content col s5">' +
+			'<p><b>Followers: </b>' + offline.followers + '</p>' +
+			'<p><b>Language: </b>' + offline.language + '</p>' +
+			'<p><b>Views: </b>' + offline.views + '</p>' +
+			'<a id="close-btn" href="#"><i id="close" class="material-icons">close</i></a>' +
+			'<a class="secondary-content"><i class="offline material-icons">play_circle_filled</i></a>' +
+			'</div>' +
+			'</li>';
+
+			$('.collection').append(html);
+		}
+		
+	}
+	function ajaxCall( api, streamer ){
 			$.ajax({
 				url: 'https://api.twitch.tv/kraken/' + api + streamer + '?callback=?',
 	    		dataType: 'jsonp',
@@ -9,100 +93,41 @@ var App = (function(){
 					if(api === 'streams/'){
 						if(data.stream === null){
 							return ajaxCall( 'channels/', streamer);
-						}	
-							var online = {
-								logo: data.stream.channel.logo,
-								displayName: data.stream.channel.display_name,
-								viewers: data.stream.viewers,
-								game: data.stream.channel.game.toUpperCase(),
-								status: data.stream.channel.status,
-								followers: data.stream.channel.followers,
-								language: data.stream.channel.language.toUpperCase(),
-								views: data.stream.channel.views,
-								channelUrl: data.stream.channel.url
-							};
-							var htmlOnline = 
-							'<li class="collection-item avatar row">' +
-							'<img src="' + online.logo + '" alt="" class="preview col s2">' +
-							'<div class="content-row col s5">' +
-							'<span id="title">' + online.displayName + '</span>' +
-							'<p><b>Viewers: </b>' + online.viewers + '</p>' +
-							'<p><b>Game: </b>' + online.game + '</p>' +
-							'<p class="status truncate">' + online.status + '</p>' +
-							'</div>' +
-							'<div class="content-row second-content col s5">' +
-							'<p><b>Followers: </b>' + online.followers + '</p>' +
-							'<p><b>Language: </b>' + online.language + '</p>' +
-							'<p><b>Views: </b>' + online.views + '</p>' +
-							'<a href="' + online.channelUrl + '" class="secondary-content"><i class="online material-icons">play_circle_filled</i></a>' +
-							'</div>' +
-							'</li>';
-
-							$('.collection').append(htmlOnline);
+						}
+						render('online', data);
 					}else{
-						var offline = {
-							logo: data.logo,
-							displayName: data.display_name,
-							game: data.game.toUpperCase(),
-							status: data.status,
-							followers: data.followers,
-							language: data.language.toUpperCase(),
-							views: data.views,
-							channelUrl: data.url
-						};
-						var htmlOffline = 
-						'<li class="collection-item avatar row">' +
-						'<img src="' + offline.logo + '" alt="" class="preview col s2">' +
-						'<div class="content-row col s5">' +
-						'<span id="title">' + offline.displayName + '</span>' +
-						'<p><b>Game: </b>' + offline.game + '</p>' +
-						'<p class="status truncate">' + offline.status + '</p>' +
-						'</div>' +
-						'<div class="content-row second-content col s5">' +
-						'<p><b>Followers: </b>' + offline.followers + '</p>' +
-						'<p><b>Language: </b>' + offline.language + '</p>' +
-						'<p><b>Views: </b>' + offline.views + '</p>' +
-						'<a href="' + offline.url + '" class="secondary-content"><i class="offline material-icons">play_circle_filled</i></a>' +
-						'</div>' +
-						'</li>';
-
-						$('.collection').append(htmlOffline);
+						render('offline', data);
 					}
 				},
 				error: function(){
-					alert( 'Error' );
+					console.log( 'Error ', streamer );
 				}
 			});
 		}
-	var duplicate = [];
-	
+
 	function ajaxSearch( query ){
 
 		function renderSearch( data ){
 			duplicate.length = 0;
 			for(var key in data.channels){
-
 				var channelName = data.channels[key].display_name;
 				//checks if the channel name contains the query
 				if(!channelName.toLowerCase().indexOf(query.toLowerCase())){
 					duplicate.push(channelName);
 					$('.results').empty();
 					for(var i = 0; i < duplicate.length; i++){
-						// ADD SEARCH SUGESTIONS HTML HERE
+						// if(streamers.forEach(indexOf(duplicate[i])){
 						$('.results').append('<li><a value="' + duplicate[i] + '" class="search-link">' + duplicate[i] + '</a></li>');
-
-						
+						// }
 					}
-					
 				}
-				
 			}
 			$('.search-link').on('click', function(){
-							var searchValue = $(this)
-								.val($(this)
-								.text());
-							$('#add-stream').val(searchValue[0].innerHTML);
-						});
+					var searchValue = $(this)
+					.val($(this)
+					.text());
+					$('#add-stream').val(searchValue[0].innerHTML);
+				});
 		}
 
 		$.ajax({
@@ -118,32 +143,51 @@ var App = (function(){
 		});
 	}
 
-	var streamers = ['freecodecamp', 'summit1g', 'tsm_dyrus', 'flosd', 'wingsofdeath'];
-	var searchResults = [];
-
 	function init(){
-
-		(function ajax(){
-			for(var key in App.streamers){
-	    		ajaxCall( 'streams/', App.streamers[key] );}
-		})();
-
-		
-
+		//render streams from the streamers array
 		$('#add-stream').keyup(function() {
-				$('.results').empty();
 				setTimeout( function(){ 
 					ajaxSearch( $('#add-stream').val() ); 
 				}, 500);
+				$('.results').empty();
 		});
 
+		$('#add-stream-label').on('click', function(){
+			$('.results').empty();
+			var value = $('#add-stream').val().toLowerCase();
+			if(value !== ''){
+				streamers.push(value);
+				localStorage.setItem("streams", JSON.stringify(streamers));
+				ajaxCall( 'streams/', value );
+				$('.results').empty();
+				$('#add-stream').val('');
+			}
+		});
+	
 
-		
+		(function ajax(){
+			for(var key in getStreamsLocal){
+				streamers.push(getStreamsLocal[key]);
+			}
+			for(var i = 0; i < getStreamsLocal.length; i++){
+	    		ajaxCall( 'streams/', getStreamsLocal[i] );
+	    	}
+	    	//Delete stream
+			$(document).on('click', '#close', function(){
+				var current = $(this).closest('li').find('#title')[0].innerHTML.toLowerCase();
+				for(var key in streamers){
+					if(current === streamers[key]){
+					streamers.splice([key], 1);
+					localStorage.setItem("streams", JSON.stringify(streamers));
+					}
+				}
+				$(this).closest('li').remove();
+			});
+			console.log(streamers, 'streamers', getStreamsLocal, 'getStreamsLocal');
+			})();
 	}
-
 	return{
-		start: init,
-		streamers: streamers
+		start: init
 	};
 })();
 
